91aefd49884a9bc4bc25f5113c467c838e82d385
KordyD <84672403+KordyD@users.noreply.github.com>
2023-09-28 17:11:19 +0300

    Connect db and use middlewares

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,33 @@
 import express from 'express';
 import { config } from 'dotenv';
 import { join } from 'path';
+import { connect } from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { router } from './routers';
+import { errorMiddleware } from './middlewares/error-middleware';
 
 config({ path: join(__dirname, '..', 'src', '.env') });
 const PORT = process.env.PORT || 3000;
+const mongoURL = process.env.MONGO_URL;
 const app = express();
+
+const start = async () => {
+  try {
+    if (mongoURL === undefined) {
+      throw new Error('No db url');
+    }
+    await connect(mongoURL, { dbName: 'JWT-auth' });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use('/api', router);
+app.use(errorMiddleware);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
